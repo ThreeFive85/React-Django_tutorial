3f20cac0139f62486cb82469f479c53f4ad74957
박동혁 <kaje0god@naver.com>
2020-09-07 21:18:02 +0900

    Fix : App.js

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import ReactDom from "react-dom";
 
+import Header from "./layout/Header";
+
 class App extends Component {
   render() {
-    return <h1>React App</h1>;
+    return (
+      <div>
+        <Header />
+      </div>
+    );
   }
 }
 
