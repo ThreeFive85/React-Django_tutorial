bed2fcb58167f1279e337ec67387065a10a10399
박동혁 <kaje0god@naver.com>
2020-12-30 17:01:16 +0900

    Feat: messages.js file  - because add create error messages

---
+++ leadmanager/frontend/src/actions/messages.js
@@ -0,0 +1,10 @@
+import { CREATE_MESSAGE } from './types';
+
+// CREATE_MESSAGES
+// 비동기 요청이지만 axios를 사용하지 않으므로 디스패치를 전달할 필요는 없다. 그래서 create message 타입을 직접 반환한다
+export const createMessage = msg => {
+    return {
+        type: CREATE_MESSAGE,
+        payload: msg
+    }
+}
