8fcbaf82361c830554159f4bded11b0667c7f364
박동혁 <kaje0god@naver.com>
2020-12-31 13:20:02 +0900

    Fix : delete GET_MESSAGES

--- leadmanager/frontend/src/reducers/messages.js
+++ leadmanager/frontend/src/reducers/messages.js
@@ -4,8 +4,8 @@ const initialState = {};
 
 export default function(state = initialState, action) {
     switch(action.type){
-        case GET_MESSAGES:
-            return action.payload;
+        // case GET_MESSAGES: 메세지를 가져오는 것은 사용하지 않으므로 case문이 필요하지는 않다.
+        //     return action.payload;
         case CREATE_MESSAGE:
             return (state = action.payload);
         default:
