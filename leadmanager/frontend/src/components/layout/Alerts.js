6b5af8c0047f20f428a6f26874d6b902a4cbe152
박동혁 <kaje0god@naver.com>
2020-12-31 12:52:59 +0900

    Fix : messages state  - add message to stattic propTypes  - add message props to componentDidUpdate

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -5,13 +5,17 @@ import PropTypes from 'prop-types';
 
 export class Alerts extends Component {
     static propTypes = {
-        error: PropTypes.object.isRequired
+        error: PropTypes.object.isRequired,
+        message: PropTypes.object.isRequired
     }
 
     componentDidUpdate(prevProps) {
-        const { error, alert } = this.props;
+        const { error, alert, message } = this.props;
         if (error !== prevProps.error) {
             if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`)
+        }
+        if (message !== prevProps.message){
+            if (message.deleteLead) alert.success(message.deleteLead);
         }
     }
     render() {
@@ -20,7 +24,8 @@ export class Alerts extends Component {
 }
 
 const mapStateToProps = state => ({
-    error: state.errors
+    error: state.errors,
+    message: state.messages
 });
 
 export default connect(mapStateToProps)(withAlert()(Alerts));
