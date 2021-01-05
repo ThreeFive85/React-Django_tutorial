fb161431a770d30683133cda44ebcd461b7c06db
박동혁 <kaje0god@naver.com>
2021-01-05 17:16:09 +0900

    Fix : LeadViewSet class  - add get_queryset function

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -1,6 +1,7 @@
 from leads.models import Lead
 from rest_framework import viewsets, permissions
 from .serializers import LeadSerializer
+from django.core.exceptions import PermissionDenied
 
 # Viewset은 View(CRUD)를 설계하는 가장 간단한 방법
 
@@ -8,8 +9,18 @@ from .serializers import LeadSerializer
 
 
 class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
-    permissions_classes = [
-        permissions.AllowAny
-    ]
+    # queryset = Lead.objects.all()
+    permissions_classes = (
+        permissions.IsAuthenticated,
+    )
+
     serializer_class = LeadSerializer
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            raise PermissionDenied()
+
+        return self.request.user.leads.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.reques.user)
