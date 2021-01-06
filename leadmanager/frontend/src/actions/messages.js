9033e10837bcd1c0bab3abe9421ad53871a63e61
박동혁 <kaje0god@naver.com>
2021-01-06 15:54:28 +0900

    Fix : messages.js  - add return errors function

--- leadmanager/frontend/src/actions/messages.js
+++ leadmanager/frontend/src/actions/messages.js
@@ -1,4 +1,4 @@
-import { CREATE_MESSAGE } from './types';
+import { CREATE_MESSAGE, GET_ERRORS } from './types';
 
 // CREATE_MESSAGES
 // 비동기 요청이지만 axios를 사용하지 않으므로 디스패치를 전달할 필요는 없다. 그래서 create message 타입을 직접 반환한다
@@ -6,5 +6,13 @@ export const createMessage = msg => {
     return {
         type: CREATE_MESSAGE,
         payload: msg
+    }
+}
+
+// RETURN ERRORS
+export const returnErrors = (msg, status) => {
+    return {
+        type: GET_ERRORS,
+        payload: {msg, status}
     }
 }
