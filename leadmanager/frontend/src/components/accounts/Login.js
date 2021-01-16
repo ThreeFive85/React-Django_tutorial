888af56fe77327e4e36a787e2676d0452012bcc1
박동혁 <kaje0god@naver.com>
2021-01-16 19:26:29 +0900

    Feat : Login.js  - write Login class code

---
+++ leadmanager/frontend/src/components/accounts/Login.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import { Link } from '@material-ui/core';
+
+export class Login extends Component {
+    state = {
+        username: '',
+        password: ''
+    }
+
+  onChangeName = (e) => this.setState({ username: e.target.value });
+  onChangePW1 = (e) => this.setState({ password: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+    // const { username, email, password, password2 } = this.state;
+    console.log('submit')
+  }
+
+  render() {
+    const { username, password } = this.state;
+    return(
+      <Typography component="div" variant="body1">
+         <Box color="success.main">
+           <div>
+             <h2>Login</h2>
+             <form onSubmit={this.onSubmit}>
+               <div className="form-group">
+                 <TextField
+                id="standard-multiline-flexible"
+                label="UserName"
+                type="text"
+                required
+                multiline
+                inputProps={{ maxLength: 10 }}
+                style={{
+                  width: '70%',
+                  marginLeft: '15%',
+                  marginRight: '15%',             
+                  paddingBottom: 10,
+                  marginTop: 0,
+                }}
+                getvalue={username}
+                onChange={this.onChangeName}
+                />
+              </div>
+              <div className="form-group">
+                <TextField
+                id="standard-multiline-flexible"
+                label="Password"
+                type="text"
+                required
+                multiline
+                inputProps={{ maxLength: 20 }}
+                style={{
+                  width: '70%',
+                  marginLeft: '15%',
+                  marginRight: '15%',             
+                  paddingBottom: 10,
+                  marginTop: 0,
+                }}
+                getvalue={password}
+                onChange={this.onChangeMessage}
+                />
+              </div>
+              <Button type='submit' color="primary" style={{marginLeft: '15%'}}>Login</Button>
+              <p align="center">
+                  Don't hava an account? <Link to="/register">Register</Link>
+              </p>
+            </form>
+          </div>
+        </Box>
+      </Typography>
+    );
+  }
+}
+
+export default Login
