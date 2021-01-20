d68900b9a0899e8590691981390df2201369fa82
박동혁 <kaje0god@naver.com>
2021-01-20 14:48:40 +0900

    Fix : App.js  - fix Dashboard route

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -7,6 +7,7 @@ import Dashboard from "./leads/Dashboard";
 import Alerts from './layout/Alerts';
 import Register from './accounts/Register';
 import Login from './accounts/Login';
+import PrivateRoute from './common/PrivateRoute';
 
 import { Provider as AlertProvider, positions } from 'react-alert';
 import AlertMUITemplate from 'react-alert-template-mui';
@@ -30,7 +31,7 @@ class App extends Component {
             <Alerts />
             <div className="container">
               <Switch>
-                <Route exact path="/" component={Dashboard} />
+                <PrivateRoute exact path="/" component={Dashboard} />
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/login" component={Login} />
               </Switch>
