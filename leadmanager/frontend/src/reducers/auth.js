56c34da910ffad39ee43dc588cf8a805fb76fdf5
박동혁 <kaje0god@naver.com>
2021-01-22 16:44:28 +0900

    Fix : auth.js  - switch action.type

--- leadmanager/frontend/src/reducers/auth.js
+++ leadmanager/frontend/src/reducers/auth.js
@@ -1,3 +1,9 @@
+import { 
+    USER_LOADING,
+    USER_LOADED,
+    AUTH_ERROR
+ } from '../actions/types';
+
 const initialState = {
     token: localStorage.getItem("token"),
     isAuthenticated: null,
@@ -7,6 +13,27 @@ const initialState = {
 
 export default function(state = initialState, action){
     switch(action.type){
+        case USER_LOADING:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case USER_LOADED: // 토큰이 정상적으로 인증되었을 때
+            return {
+                ...state,
+                isAuthenticated: true,
+                isLoading: false,
+                user: action.payload
+            }
+        case AUTH_ERROR:
+            localStorage.removeItem('token');
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                isLoading: false,
+                user: null
+            }
         default:
             return state;
     }
