fb4b82109df22059e22b8df66f80ce8ec57da2bb
박동혁 <kaje0god@naver.com>
2021-01-28 16:15:31 +0900

    Fix : Link

--- leadmanager/frontend/src/components/accounts/Register.js
+++ leadmanager/frontend/src/components/accounts/Register.js
@@ -3,7 +3,7 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import { Link } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 export class Register extends Component {
     state = {
